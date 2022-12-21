f187b7cf7268a5ea2df1b429abfee27221b38fe8
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-21 17:36:16 +0100

    build devel

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd loan-calculator-app/loan-calculator-backend
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle build --nodaemon
+cd ../loan-calculator-app/loan-calculator-backend
+docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle clean build --no-daemon
 docker build . --tag loan-calculator-backend:latest
