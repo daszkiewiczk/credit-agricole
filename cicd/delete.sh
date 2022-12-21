e4810a5b8c823d53fdc92809353e041e74e324aa
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-21 22:19:45 +0100

    persistence container

--- cicd/delete.sh
+++ cicd/delete.sh
@@ -1,3 +1,4 @@
 kubectl delete deployment loan-calculator-backend
+kubectl delete deployment postgres-deployment 
 kubectl delete svc loan-calculator-backend-service
 kind delete cluster --name kazik
--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,5 +1,10 @@
 kind load docker-image daszkiewiczk/loan-calculator-backend:latest --name kazik
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-deployment.yaml 
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-service.yaml 
+kubectl apply -f ../loan-calculator-app/loan-calculator-backend/ingress.yaml
+
+kubectl apply -f ../loan-calculator-app/loan-calculator-backend/postgres-pvc.yaml
+kubectl apply -f ../loan-calculator-app/loan-calculator-backend/database-deployment.yaml
+
 
 
