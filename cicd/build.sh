4bf92a97d95507919638e8f9880d52701e68df61
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-21 18:46:27 +0100

    build, deploy devel

--- cicd/build.sh
+++ cicd/build.sh
@@ -2,4 +2,6 @@
 
 cd ../loan-calculator-app/loan-calculator-backend
 docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle clean build --no-daemon
-docker build . --tag loan-calculator-backend:latest
+docker build . --tag daszkiewiczk/loan-calculator-backend:latest
+
+
---
+++ cicd/deploy.sh
@@ -0,0 +1,3 @@
+kind load docker-image daszkiewiczk/loan-calculator-backend:latest --name kazik
+kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-deployment.yaml 
+
