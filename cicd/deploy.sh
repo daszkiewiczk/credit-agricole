c401db9b057e1d5c6e342d9cca8d89e05273bdbe
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-23 18:30:16 +0100

    refactor

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,4 +1,4 @@
 kind load docker-image daszkiewiczk/loan-calculator-backend:latest --name kazik
 kind load docker-image daszkiewiczk/loan-calculator-frontend:latest --name kazik
 
-kubectl apply -f ../k8s/*.yaml
+kubectl apply -f ../k8s/
