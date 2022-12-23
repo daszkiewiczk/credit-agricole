4836dc42acb33b7cc7a8060bf8e40eb0c09d924c
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-23 14:23:52 +0100

    added frontend Dockerfile, deployment and service

--- cicd/delete.sh
+++ cicd/delete.sh
@@ -1,4 +1,10 @@
 kubectl delete deployment loan-calculator-backend
-kubectl delete deployment postgres-deployment 
 kubectl delete svc loan-calculator-backend-service
+
+kubectl delete deployment postgres-deployment
+kubectl delete svc postgres-service
+
+kubectl delete deployment frontend-deployment
+kubectl delete deployment frontend-service
+
 kind delete cluster --name kazik
--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,4 +1,5 @@
 kind load docker-image daszkiewiczk/loan-calculator-backend:latest --name kazik
+kind load docker-image daszkiewiczk/loan-calculator-frontend:latest --name kazik
 
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-deployment.yaml 
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-service.yaml
@@ -8,6 +9,9 @@ kubectl apply -f ../loan-calculator-app/loan-calculator-backend/ingress.yaml
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/postgres-pvc.yaml
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/database-deployment.yaml
 kubectl apply -f ../loan-calculator-app/loan-calculator-backend/database-service.yaml
+
+kubectl apply -f ../loan-calculator-app/loan-calculator-frontend/frontend-deployment.yaml
+kubectl apply -f ../loan-calculator-app/loan-calculator-frontend/frontend-service.yaml
 
 
 
