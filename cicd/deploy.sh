efd494d07e492ebf947eefd4bd16271b5c8267a8
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-23 18:27:32 +0100

    refactor

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,17 +1,4 @@
 kind load docker-image daszkiewiczk/loan-calculator-backend:latest --name kazik
 kind load docker-image daszkiewiczk/loan-calculator-frontend:latest --name kazik
 
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-deployment.yaml 
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/backend-service.yaml
-
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/ingress.yaml
-
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/postgres-pvc.yaml
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/database-deployment.yaml
-kubectl apply -f ../loan-calculator-app/loan-calculator-backend/database-service.yaml
-
-kubectl apply -f ../loan-calculator-app/loan-calculator-frontend/frontend-deployment.yaml
-kubectl apply -f ../loan-calculator-app/loan-calculator-frontend/frontend-service.yaml
-
-
-
+kubectly apply -f ../k8s/*.yaml
