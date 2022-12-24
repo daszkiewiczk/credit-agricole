9bc75b35e81254d3498a95c9c32b3d1872a1a873
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-24 11:29:33 +0100

    updated setup.sh

--- cicd/setup.sh
+++ cicd/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+#create the cluster
 kind create cluster --name kazik --config ../cluster.yaml
 
+#create seperate namespaces for application and monitoring
 kubectl create namespace loan-calculator
+kubectl create namespace loan-calculator-monitoring
 
 #make ingress possible
 kubectl apply -f https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/master/deploy/single/all-in-one-dbless.yaml
@@ -11,9 +15,10 @@ kubectl patch service -n kong kong-proxy -p '{"spec":{"type":"NodePort"}}'
 #create argocd
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd patch service argocd-server -p '{"spec":{"type":"NodePort"}}'
-
+kubectl -n argocd patch service argocd-server -p '{"spec":{"type":"NodePort"}}' #makes the dashboard accessible
 #define argocd objects
-kubectl apply -f argocd-repository.yaml
-kubectl apply -f argocd-application.yaml
+kubectl apply -f argocd-repository.yaml #repository secret 
+kubectl apply -f argocd-application.yaml 
+
+
 
