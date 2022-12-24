b90bf25b4ec16605dde10aaba1118fb4c6e93779
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-24 01:40:45 +0100

    create app namespace

--- cicd/setup.sh
+++ cicd/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kind create cluster --name kazik --config ../cluster.yaml
 
+kubectl create namespace loan-calculator
+
 #make ingress possible
 kubectl apply -f https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/master/deploy/single/all-in-one-dbless.yaml
 kubectl patch deployment -n kong ingress-kong -p '{"spec":{"template":{"spec":{"containers":[{"name":"proxy","ports":[{"containerPort":8000,"hostPort":80,"name":"proxy","protocol":"TCP"},{"containerPort":8443,"hostPort":443,"name":"proxy-ssl","protocol":"TCP"}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
