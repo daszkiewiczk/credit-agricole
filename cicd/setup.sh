1c20ace01abd6a0acac5d586d75ac8958420c884
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-24 01:16:20 +0100

    added argocd

--- cicd/setup.sh
+++ cicd/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 kind create cluster --name kazik --config ../cluster.yaml
+
+#make ingress possible
 kubectl apply -f https://raw.githubusercontent.com/Kong/kubernetes-ingress-controller/master/deploy/single/all-in-one-dbless.yaml
 kubectl patch deployment -n kong ingress-kong -p '{"spec":{"template":{"spec":{"containers":[{"name":"proxy","ports":[{"containerPort":8000,"hostPort":80,"name":"proxy","protocol":"TCP"},{"containerPort":8443,"hostPort":443,"name":"proxy-ssl","protocol":"TCP"}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
 kubectl patch service -n kong kong-proxy -p '{"spec":{"type":"NodePort"}}'
+
+#create argocd
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd patch service argocd-server -p '{"spec":{"type":"NodePort"}}'
+
