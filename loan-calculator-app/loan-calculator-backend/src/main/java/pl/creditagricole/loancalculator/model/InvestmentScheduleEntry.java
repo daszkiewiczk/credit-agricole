54cc3199ba2f86f18932169be5eddf1aeaeb76b3
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2022-12-27 12:59:47 +0100

    dev

--- loan-calculator-app/loan-calculator-backend/src/main/java/pl/creditagricole/loancalculator/model/InvestmentScheduleEntry.java
+++ loan-calculator-app/loan-calculator-backend/src/main/java/pl/creditagricole/loancalculator/model/InvestmentScheduleEntry.java
@@ -4,31 +4,11 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 
-public class InvestmentScheduleEntry {
-    private Date paymentDate;
-    private BigDecimal capitalPart;
-    private BigDecimal interestPart;
-    private BigDecimal outstandingPrincipal;
+public class InvestmentScheduleEntry extends ScheduleEntry {
 
     public InvestmentScheduleEntry(Date paymentDate, BigDecimal capitalPart, BigDecimal interestPart, BigDecimal outstandingPrincipal) {
-        this.paymentDate = paymentDate;
-        this.capitalPart = capitalPart;
-        this.interestPart = interestPart;
-        this.outstandingPrincipal = outstandingPrincipal;
+        super(paymentDate, capitalPart, interestPart, outstandingPrincipal);
     }
 
-    public Date getPaymentDate() {
-        return paymentDate;
-    }
 
-    public BigDecimal getCapitalPart() {
-        return capitalPart;
-    }
-    public BigDecimal getInterestPart() {
-        return interestPart;
-    }
-
-    public BigDecimal getOutstandingPrincipal() {
-        return outstandingPrincipal;
-    }
 }
